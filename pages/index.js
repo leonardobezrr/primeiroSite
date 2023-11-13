751820c244a5fd2aad4f2065a18a74d9108993a6
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-11-13 15:30:38 -0300

    quebrando linha

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home (){
         <h1>Site PWEB</h1>
         <a href='https://github.com/leonardobezrr/PWEB1' target='blank'>GitHub</a>
         <h3>Receitas</h3>
-        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita1' target='blank'>Receita 1</a>
-        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita2' target='blank'>Receita 2</a>
+        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita1' target='blank'>Receita 1</a><br></br>
+        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita2' target='blank'>Receita 2</a><br></br>
         <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita3' target='blank'>Receita 3</a>
     </div>
     )
