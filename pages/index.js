b8885c4ff5086147712353370b8a620b5623fbd2
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-11-13 15:44:20 -0300

    recorrigindo o código

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-
-import "./style.css"
-
 export default function Home (){
     return (
     <div>
         <h1>Site PWEB</h1>
-        <a href='https://github.com/leonardobezrr/PWEB1' target='blank'>GitHub</a>
-        <h3>Receitas</h3>
+        <a class="uncolored-link" href='https://github.com/leonardobezrr/PWEB1' target='blank'>GitHub</a>
+        <h3>Receitas NEXT</h3>
         <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita1' target='blank'>Receita 1</a><br></br>
         <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita2' target='blank'>Receita 2</a><br></br>
         <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita3' target='blank'>Receita 3</a>
