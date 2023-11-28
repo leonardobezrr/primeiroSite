016858792157a8b3ce7c640cf4bd0a86d23f9b4b
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-11-28 11:05:15 -0300

    corrigindo links e formatação

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ export default function Home (){
     return (
     <div>
         <h1>Site PWEB</h1>
-        <a class="uncolored-link" href='https://github.com/leonardobezrr/PWEB1' target='blank'>GitHub</a>
+        <a class="uncolored-link" href='https://github.com/leonardobezrr/PWEB1/tree/main/next0' target='blank'>GitHub</a>
         <h3>Receitas NEXT</h3>
-        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita1' target='blank'>Receita 1</a><br></br>
-        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita2' target='blank'>Receita 2</a><br></br>
-        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita3' target='blank'>Receita 3</a>
-        <a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita4' target='blank'>Receita 4</a>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita1' target='blank'>Receita 1</a></p>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita2' target='blank'>Receita 2</a></p>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita3' target='blank'>Receita 3</a></p>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita4' target='blank'>Receita 4</a></p>
 
     </div>
     )
