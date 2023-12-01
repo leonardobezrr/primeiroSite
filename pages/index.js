1fd864d473d8d80fe8bab63894e0579cfb3a70b5
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-11-30 23:01:24 -0300

    upando receitas

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,14 @@ export default function Home (){
         <h1>Site PWEB</h1>
         <a class="uncolored-link" href='https://github.com/leonardobezrr/PWEB1/tree/main/next0' target='blank'>GitHub</a>
         <h3>Receitas NEXT</h3>
-        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita1' target='blank'>Receita 1</a></p>
-        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita2' target='blank'>Receita 2</a></p>
-        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita3' target='blank'>Receita 3</a></p>
-        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita4' target='blank'>Receita 4</a></p>
+        <h4>Receita 1</h4>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita1' target='blank'>Código - Git Hub</a> / <a href="./receita1/principal" target="blank">Receita no ar</a></p>
+        <h4>Receita 2</h4>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita2' target='blank'>Código Git Hub</a> / <a href="./receita2/novaPagina" target="blank">Receita no ar</a></p>
+        <h4>Receita 3</h4>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita3' target='blank'>Código Git Hub</a> / <a href="./receita3/movies" target="blank">Receita no ar</a></p>
+        <h4>Receita 4</h4>
+        <p><a href='https://github.com/leonardobezrr/PWEB1/tree/main/next0/pages/receita4' target='blank'>Código Git Hub</a> / <a href="./receita4/app" target="blank">Receita no ar I</a> / <a href="./receita4/movies2" target="blank">Receita no ar II</a></p>
 
     </div>
     )
